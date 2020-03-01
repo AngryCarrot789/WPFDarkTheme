1c5f67ec0ba04a4975b34b0f23316fe8c0343111
REghZybbbbbbbbbb  w <32274404+AngryCarrot789@users.noreply.github.com>
2020-03-01 23:16:43 +0000

    upgraded tabitem selected background

---
+++ Themes/ThemesFolder/DarkTheme.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Themes.ThemesFolder
+{
+    public partial class DarkTheme
+    {
+        private void eee(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("eee");
+        }
+    }
+}
