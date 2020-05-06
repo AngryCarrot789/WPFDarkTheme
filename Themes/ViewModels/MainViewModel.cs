01400070cb6c798a6d51588380de16b857c3c29e
REghZy <32274404+AngryCarrot789@users.noreply.github.com>
2020-05-06 16:42:29 +0100

    Updated the themes folder to make it better. also improved colourful light theme

--- Themes/ViewModels/MainViewModel.cs
+++ Themes/ViewModels/MainViewModel.cs
@@ -11,37 +11,8 @@ using Themes.Utilities;
 
 namespace Themes.ViewModels
 {
-    public class DataGridItemFileExplorerSortOf
-    {
-        public string FileName { get; set; }
-        public string DateModified { get; set; }
-        public string FileType { get; set; }
-        public string FileSize { get; set; }
-    }
-
     public class MainViewModel : BaseViewModel
     {
-        private int _themeIndex;
-        public int ThemeIndex { get => _themeIndex; set { RaisePropertyChanged(ref _themeIndex, value); SetTheme(); } }
-        public MainWindow MainWindow { get; set; }
-        public MainViewModel(MainWindow mainWindow)
-        {
-            MainWindow = mainWindow;
-            ThemeIndex = 3;
-        }
 
-        public void SetTheme()
-        {
-            if (MainWindow != null)
-            {
-                switch (ThemeIndex)
-                {
-                    case 0: MainWindow.SetTheme(MainWindow.Theme.Light); break;
-                    case 1: MainWindow.SetTheme(MainWindow.Theme.Dark); break;
-                    case 2: MainWindow.SetTheme(MainWindow.Theme.ColourfulDark); break;
-                    case 3: MainWindow.SetTheme(MainWindow.Theme.ColourfulLight); break;
-                }
-            }
-        }
     }
 }
