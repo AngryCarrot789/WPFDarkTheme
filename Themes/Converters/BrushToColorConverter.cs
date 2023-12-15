6164ad960374b3dc6543a58476cfca11fc3d43d7
Derek Gooding <djgooding@live.com>
2023-12-15 09:53:57 -0500

    Fixed a runtime error
    Error: Cannot create a default one-way converter.
    
    So I created one.

---
+++ Themes/Converters/BrushToColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Themes.Converters;
+
+public class BrushToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is SolidColorBrush brush)
+        {
+            return brush.Color;
+        }
+        return Colors.Transparent; // Default value if conversion fails
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Color color)
+        {
+            return new SolidColorBrush(color);
+        }
+        return Brushes.Transparent; // Default value if conversion fails
+    }
+}
