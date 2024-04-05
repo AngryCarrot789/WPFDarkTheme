7cb13f2b7a5888eee15aa1826506a37cd784c57e
REghZy <32274404+AngryCarrot789@users.noreply.github.com>
2024-04-05 12:21:15 +0100

    Added avalonia theme files, with a button theme so far

---
+++ Theme.Avalonia/Theme.Avalonia/Themes/JetTheme.axaml.cs
@@ -0,0 +1,10 @@
+﻿using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+using System;
+
+namespace SharpPad.Themes;
+
+public class JetTheme : Styles
+{
+    public JetTheme(IServiceProvider? sp = null) => AvaloniaXamlLoader.Load(sp, this);
+}
